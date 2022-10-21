fb929c28c54774624047f623f38bea9681ca09d7
chaemin <w2114@e-mirim.hs.kr>
2022-10-21 14:40:43 +0900

    모듈 실습

--- module_study/sayHi.js
+++ module_study/sayHi.js
@@ -1,5 +1,14 @@
 // 함수를 외부에서 쓰기 위해 export 사용
-export function sayHi(target) {
+function sayHi(target) {
 	alert(target);
 	return target;
 }
+
+function sayBye(user) {
+    alert(`Bye, ${user}!`);
+}
+
+let hello = "hellooooooooo";
+
+export default sayHi;
+export {sayBye, hello};
